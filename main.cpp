fc7efd21c43be67949ff37c23d6cd9414ffb18df
Andrey Rodriguez <andrey.gz.2016@gmail.com>
2022-07-03 01:29:22 -0400

    0.8.1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 using namespace std;
-// estructura empresa
 //hacer una matriz de 8x30
 int matriz[8][30];
-
-int contador = 0;
-//funcion para llenar la matriz
+//llenar la matriz
 void llenarMatriz(){
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 30; j++) {
@@ -14,7 +11,7 @@ void llenarMatriz(){
     }
 
 }
-//funcion para imprimir la matriz
+//imprimir la matriz
 void imprimirMatriz(){
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 30; j++) {
@@ -23,93 +20,88 @@ void imprimirMatriz(){
         cout << endl;
     }
 }
-//Anadir la produccion diaria de un area especifica
+// 1  anadir la produccion diaria de un area especifica
 void addPodrucD(){
     int area;
     int dia;
-       cout << "ingrese el area y el dia: ";
-       cin>>area>>dia;
-         cout<<"ingrese la produccion: ";
-            cin>>matriz[area-1][dia-1];
+    cout << "ingrese el area y el dia: ";
+    cin>>area>>dia;
+    cout<<"ingrese la produccion: ";
+    cin>>matriz[area-1][dia-1];
 }
-//mostar produccion diaria de todas las areas un dia especifico
-void mostrarDia(){
+// 2 Enviar a mantenimiento un area especifica
+void mant() {
+    int area;
+    cout << "ingrese el area a mandar a mantenimiento: ";
+    cin >> area;
+    for (int j = 0; j < 30; ++j) {
+        if (matriz[area - 1][j] < 4000 && matriz[area - 1][j] != 0) {
+            matriz[area - 1][j+1] = 0;
+        }
+    }
+}
+
+// 3 produccion total diaria de todas las areas un dia especifico
+void totalDia(){
     int dia;
+    int total=0;
     cout<<"ingrese el dia: ";
     cin>>dia;
     for (int i = 0; i < 8; i++) {
-        cout<<"area "<<i+1<<": "<<matriz[i][dia-1]<<endl;
+        total+=matriz[i][dia-1];
     }
+    cout<<"la produccion total del dia "<<dia<<" es: "<<total<<endl;
 }
-//Enviar a mantenimiento
-void mant(){
-    int area;
-    cout<<"ingrese el area a mandar a mantenimiento: ";
-    cin>>area;
-    for (int i = 0; i < 8; ++i) {
+
+// 4 contar areas en mantenimiento
+void contarMant(){
+    int cont = 0;
+    for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 30; j++) {
-            if (matriz[area-1][j]==0){
-
-
-}else if(matriz[area-1][j]<4000){
-                matriz[i][j+1]=0;
+            if (matriz[i][j] == 0) {
+                cont++;
             }
         }
     }
+    cout << "las areas en mantenimiento son: " << cont << endl;
 }
-
-
-
-void menu(){
-    int opcion;
-    //iteracion del menu
-    while(opcion!=4){
-        cout<<"Opciones"<<endl;
-        cout<<"1. Anadir la produccion diaria de un area especifica"<<endl;
-        cout<<"2. Cuantas estan en mantenimiento"<<endl;
-        cout<<"3. Salir"<<endl;
-        cout<<"Seleccione una opcion: ";
-        cin>>opcion;
-        switch(opcion){
-            case 1:
-                //llamar a la funcion para llenar la matriz
-                llenarMatriz();
-                //hacer un contador para saber cuantas veces se debe de mandar a mantenimiento
-
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 30; j++) {
-                        if(matriz[i][j] <4000){
-                            contador++;
-                        }
-                    }
-                }
-                cout<<"se debe de mandar a mantenimiento "<<contador<<"veces"<<endl;
-                break;
-            case 2:
-                //llamar a la funcion para llenar la matriz
-                llenarMatriz();
-                //hacer un contador para saber cuantas esta en mantenimiento
-                for (int i = 0; i < 8; i++) {
-                    for (int j = 0; j < 30; j++) {
-                        if(matriz[i][j] ==0){
-                            contador++;
-                        }
-                    }
-                }
-                cout<<"se encuentran en mantenimiento "<<contador<<endl;
-                break;
-
-            case 3:
-                cout<<"Saliendo..."<<endl;
-                break;
-            default:
-                cout<<"Opcion no valida"<<endl;
-                cout<<"Seleccione una opcion: ";
-                cin>>opcion;
-                break;
+// 5 porciento de areas en mantenimiento
+void porcentajeMant(){
+    int cont = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 30; j++) {
+            if (matriz[i][j] == 0) {
+                cont++;
+            }
         }
     }
+    cout << "el porcentaje de areas en mantenimiento es: " << cont * 100 / 64 << endl;
 }
+//  6 mostrar areas que sobrepasan 9000 almenos 3 veces
+void sobrepasan(){
+   int cont = 0;
+   int cont2 = 0;
+   int cont3 = 0;
+   int cont4 = 0;
+   int cont5 = 0;
+   int cont6 = 0;
+   int cont7 = 0;
+   int cont8 = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 30; j++) {
+            if (matriz[0][j] > 9000) {
+                cont++;
+            }else if(matriz[1][j] > 9000){
+                cont2++;
+            }else if(matriz[2][j] > 9000){
+                cont3++;
+        }
+    }
+    cout << "las areas que sobrepasan 9000 son: " << cont << endl;
+}
+
+//menu
+
 int main(){
     llenarMatriz();
     mant();
